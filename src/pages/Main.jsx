6a5538a95cc4bca6073c547f430de2ac44018227
Sanjeev Kushwaha <sanjeevk249116@gmail.com>
2024-02-01 17:11:08 +0530

    main Part

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
-
+import { FaRegBell } from 'react-icons/fa'
+import { HiOutlineUpload } from "react-icons/hi";
 function Main() {
   return (
+    <div className='mainPart'>
+   <div className='navbar'>
     <div>
-      
+        <p>Upload CSV</p>
+    </div>
+    <div className='right_part'>
+       <div>
+       <FaRegBell size={18} />
+        </div> 
+        <div>
+            <img src="https://s3-alpha-sig.figma.com/img/203e/bb33/eccd71f34638f7751900105c639d563d?Expires=1707696000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=k9saXYAamsOvnZ1xhzIr2V29Xg9PlJAWrA5JwbJGAigpiUSMK~I3gQykef14hsTkdgIetuFCvO3Vk2H9v7WDmDEj2~pGdVK4Llmwe7VF4VRESG80qqKTmpNJkBxo2ZGgUyk5eMGuphGBrqeklJDHDEvnitPyUXRyjKNEPuNmjKBKnRi2dX9Z~0JPQ7BtRQyIG79BojPFd0MMLKEzGa58ZjHmqHMNHzZMK633TpcnU0v1ulRwOLVsupz-Qwvbw69d33Dubm4VZv9zlh5Gs8~h9O8yT99au9O~LJvfq3p6fKRKdd--Aum4KJAJbiHEScNdcpij4vB8Pd~VVUicLKSGEA__" alt="logo_img" />
+        </div>
+    </div>
+   </div>
+   <div className='drop_part'>
+    <div className='inerPart'>
+        <img src="https://i.ibb.co/pzLQHXG/uploader.png" alt="uploader" />
+    </div>
+    <button className='btn1'> <span><HiOutlineUpload /></span>Upload</button>
+   </div>
     </div>
   )
 }
--- src/pages/SideBar.jsx
+++ src/pages/SideBar.jsx
@@ -1,9 +1,62 @@
 import React from 'react'
-
+import { CgLoadbarSound } from "react-icons/cg";
+import { RxDashboard } from "react-icons/rx";
+import { FaFileInvoice } from "react-icons/fa";
+import { AiFillSchedule } from "react-icons/ai";
+import { SlCalender } from "react-icons/sl";
+import { RiNotification4Fill } from "react-icons/ri";
+import { IoSettingsOutline } from "react-icons/io5";
 function SideBar() {
   return (
-    <div>
-    
+    <div className='sidebar'>
+  <div >
+    <img src="https://i.ibb.co/gjrzrzL/mainlogo.png" alt="mainlogo" />
+    <p>Base</p>
+  </div>
+<div className='sidebar_mid'>
+    <div className='main1' >
+        <div>
+        <RxDashboard size={24} />
+        </div>
+        <div>Dashboard</div>
+    </div>
+    <div className='main1 ' >
+        <div className='main2'>
+        <CgLoadbarSound size={24}/>
+        </div>
+        <div className='upload'>Upload</div>
+    </div>
+    <div className='main1' >
+        <div>
+        <FaFileInvoice size={24}/>
+        </div>
+        <div>Invoice</div>
+    </div>
+    <div className='main1' >
+        <div>
+        <AiFillSchedule size={24}/>
+        </div>
+        <div>Schedule</div>
+    </div>
+    <div className='main1' >
+        <div>
+        <SlCalender size={24}/>
+        </div>
+        <div>Calender</div>
+    </div>
+    <div className='main1' >
+        <div>
+        <RiNotification4Fill size={24}/>
+        </div>
+        <div>Notification</div>
+    </div>
+    <div className='main1' >
+        <div>
+        <IoSettingsOutline size={24}/> 
+        </div>
+        <div>Setting</div>
+    </div>
+</div>
     </div>
   )
 }
